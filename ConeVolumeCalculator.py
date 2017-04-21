3edb2589c0ac6691fd9344ca50b0c21fbd64998c
Brookins_Kian <brook085@hsd.k12.or.us>
2017-04-21 11:33:23 -0700

    Solved CodeVolumeCalculator

--- ConeVolumeCalculator.py
+++ ConeVolumeCalculator.py
@@ -3,11 +3,16 @@
 # it **calculateConeVolume()** that calculates the volume of a cone
 # factor based on the Volume Calculator
 # Calculator.net (http://www.calculator.net/volume-calculator.html)
+import math
 
 # Define Function below
 # be sure to return an integer
+def calculateConeVolume(r, h):
 
-
+    volume = 1/3*math.pi*r**2*h
+    volume = round (volume, 2)
+    return volume
+    
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
